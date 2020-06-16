293a2074b6898b22cd0c15aedb93355a074d2e07
Laurent Toulouse <toulouselaurent0@gmail.com>
2020-06-16 20:57:45 +0200

    Ajout de la fonctionnalité de récupération des données du formulaire vers un fichier json

--- src/Model/ContactManager.php
+++ src/Model/ContactManager.php
@@ -26,34 +26,34 @@ class ContactManager
      */
     public function insert(array $contact)
     {
-            $err = "";
-            $locationJson = __DIR__. self::FILE ;
-            $arrData = array();
-            
-            //Get data from existing json file
-            $jsondata = file_get_contents($locationJson);
+        $err = "";
+        $locationJson = __DIR__. self::FILE ;
+        $arrData = array();
+        
+        //Get data from existing json file
+        $jsondata = file_get_contents($locationJson);
     
-            if(empty ($jsondata)){
-                $err = "le fichier est vide";
-            }
-            // converts json data into array
-            $arrData = json_decode($jsondata, true);
-            if(isset ($arrData)){
-                $err = "Impossible de convertir les données en tableau";
-            }
-            // Push user data to array
-            array_push($arrData, $contact);
-    
-            //Convert updated array to JSON
-            $jsondata = json_encode($arrData, JSON_PRETTY_PRINT);
-            if(isset ($jsondata)){
-                $err = "Impossible de convertir les données en json";
-            }
-            //write json data into contact.json file
-            if (file_put_contents($locationJson, $jsondata) && $err === "") {
-                echo 'Data successfully saved';
-            } else {
-                echo $err;
-            }
+        if (empty($jsondata)) {
+            $err = "le fichier est vide";
+        }
+        // converts json data into array
+        $arrData = json_decode($jsondata, true);
+        if (isset($arrData)) {
+            $err = "Impossible de convertir les données en tableau";
+        }
+        // Push user data to array
+        array_push($arrData, $contact);
+
+        //Convert updated array to JSON
+        $jsondata = json_encode($arrData, JSON_PRETTY_PRINT);
+        if (empty($jsondata)) {
+            $err = "Impossible de convertir les données en json";
+        }
+        //write json data into contact.json file
+        if (file_put_contents($locationJson, $jsondata) & $err === "") {
+            echo 'Data successfully saved';
+        } else {
+            echo $err;
+        }
     }
 }
