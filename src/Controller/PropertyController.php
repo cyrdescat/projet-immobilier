46472c32e772735eeaf972c8afa78ebccd7178c1
Laurent Toulouse <toulouselaurent0@gmail.com>
2020-06-18 09:54:01 +0200

    ajout page détail property showOne

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -221,16 +221,15 @@ class PropertyController extends AbstractController
         $propertyManager = new PropertyManager();
         $property = $propertyManager->selectAllFromOne($_GET["id"]);
         $pictures = $propertyManager->selectPicturesFromOne($_GET["id"]);
-        $agences = $propertyManager->selectAgenceFromOne($_GET["id"]);        
+        $agences = $propertyManager->selectAgenceFromOne($_GET["id"]);
         $pageURL = strtok($_SERVER['REQUEST_URI'], '?');
 
         return $this->twig->render('/Property/showOne.html.twig', [
-            'property' => $property, 
-            'pictures' => $pictures, 
+            'property' => $property,
+            'pictures' => $pictures,
             'agences' => $agences,
             'pageURL' => $pageURL
             ]);
-
     }
 
     /**
